b176c23be78804b11d577d940f3da3011c2d1796
Megha <megha.agarwal@somaiya.edu>
2020-08-01 16:31:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/navapp/contactHandler.java
@@ -0,0 +1,14 @@
+package com.example.navapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class contactHandler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_handler);
+    }
+}
